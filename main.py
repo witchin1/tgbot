98d9886743cab031561c8e073b7763efeb90e93f
witchin1 <111995048+witchin1@users.noreply.github.com>
2022-08-25 14:20:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
 @bot.message_handler(commands=['start', 'help']) #декоратор, функция, которая внутри библиотеки
 def say_hi(message):
     print(message)
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Привет! Как дела?")
 
 @bot.message_handler(func=lambda m: True)
 def my_destiny(message):
